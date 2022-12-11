0324f97c14830e9852105396a7182e0df58bed33
Mateus-Gabardo <gabardomateus@gmail.com>
2022-12-11 14:35:14 -0300

    Add função de arquivo

---
+++ app.py
@@ -0,0 +1,8 @@
+import commonFunction
+
+#####################################################
+# Heuristica construtiva
+#####################################################
+
+
+nbm, nbj, p_ij = commonFunction.read_from_file("example3.txt")
--- commonFunction.py
+++ commonFunction.py
@@ -1,3 +1,15 @@
+
 #Retorna as intancias do problema
 def instances():
     return '';
+
+def read_from_file(arquivo):
+    with open(arquivo, "r") as fic:
+        my_nbm = int(fic.readline())
+        my_nbj = int(fic.readline())
+        my_p_ij = None
+        for i in range(my_nbm):
+            p_i = fic.readline().split()
+            for j in range(my_nbj):
+                my_p_ij[i][j] = int(p_i[j])
+    return my_nbm, my_nbj, my_p_ij
