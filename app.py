eb11e9625c645a7afd779f2a8eda183338b34289
Mateus-Gabardo <gabardomateus@gmail.com>
2022-12-11 15:31:39 -0300

    exluido metodos

--- app.py
+++
@@ -1,8 +0,0 @@
-import commonFunction
-
-#####################################################
-# Heuristica construtiva
-#####################################################
-
-
-nbm, nbj, p_ij = commonFunction.read_from_file("example3.txt")
