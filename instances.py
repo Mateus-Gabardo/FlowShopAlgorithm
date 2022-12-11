b54bd5a7b8f70d2d0db8f35ab80d2cd4c0a48c45
Daniel Larion Klug <dklug22@gmail.com>
2022-12-11 15:23:47 -0300

    Leitor de instâncias e instâncias adicionadas

---
+++ instances.py
@@ -0,0 +1,30 @@
+def getInstances():
+    file = open ("./instances.txt", 'r')
+    list = file.read().splitlines()
+    return list
+
+def extract_settings(line):
+    content = line.split('=')[-1]
+    return content.split('=')
+
+def extract_array(line):
+    content = line.split('=')[-1]
+    array = content.split(',')
+    return array
+
+def generate(instance):
+    global nbj, nbm, seed, obj, lowObj, array
+    array = []
+    instances = getInstances()
+    file = open(instances[instance], 'r')
+    for line in file.readlines():
+        line = line.strip()
+        if line != '' and line[0] != '#':
+            if 'numberOfJobs' in line: nbj = extract_settings(line)
+            if 'numberOfMachines' in line: nbm = extract_settings(line)
+            if 'initialSeed' in line: seed = extract_settings(line)
+            if 'upperBound' in line: obj = extract_settings(line)
+            if 'lowerBound' in line: lowObj = extract_settings(line)
+            if 'array' in line: pass
+            if 'line' in line: array.append(extract_array(line))
+    file.close()
